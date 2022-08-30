df7f2bfb69916007ea599eeb4055acdbe50b4f9a
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2022-08-30 16:17:05 -0700

    redeploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import {defineConfig} from "vite"
 
 export default defineConfig({
 
-    base: "/RUNNING-SQUARE",
+    base: "/RUNNING-SQUARE/",
     build:{
         chunkSizeWarningLimit:1600,
     }
